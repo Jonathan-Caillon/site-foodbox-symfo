c8a61ec1f9754f990d1ae87441696318bf6d7f03
Jonathan-Caillon <j.caillon@codeur.online>
2022-02-21 15:35:54 +0100

    contact

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AccueilRepository;
+use App\Repository\ContactRepository;
 use App\Repository\GalerieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +13,13 @@ use App\Repository\ProduitRepository;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'home_page')]
-    public function index(ProduitRepository $produitRepository, GalerieRepository $galerieRepository, AccueilRepository $accueilRepository): Response
+    public function index(ProduitRepository $produitRepository, GalerieRepository $galerieRepository, AccueilRepository $accueilRepository, ContactRepository $contactRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
             'produits' => $produitRepository->findAll(),
             'galeries'=> $galerieRepository->findAll(),
             'accueils'=> $accueilRepository->findAll(),
+            'contacts'=> $contactRepository->findAll(),
         ]);
     }
 }
